4c7ca45bec8b790b2ea238c11111689c7d0e3254
wutsi <herve.tchepannou@wutsi.com>
2022-01-29 22:34:12 -0500

    fix test

--- src/test/kotlin/com/wutsi/application/store/endpoint/settings/screen/SettingsStoreScreenTest.kt
+++ src/test/kotlin/com/wutsi/application/store/endpoint/settings/screen/SettingsStoreScreenTest.kt
@@ -1,6 +1,12 @@
 package com.wutsi.application.store.endpoint.settings.screen
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.application.store.endpoint.AbstractEndpointTest
+import com.wutsi.platform.catalog.dto.PictureSummary
+import com.wutsi.platform.catalog.dto.ProductSummary
+import com.wutsi.platform.catalog.dto.SearchProductResponse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -21,5 +27,12 @@ internal class SettingsStoreScreenTest : AbstractEndpointTest() {
     }
 
     @Test
-    fun index() = assertEndpointEquals("/screens/settings/store/store.json", url)
+    fun index() {
+        val product1 = ProductSummary(id = 1, title = "1")
+        val product2 = ProductSummary(id = 2, title = "2")
+        val product3 = ProductSummary(id = 3, title = "3", thumbnail = PictureSummary(url = "http://u.com/1.png"))
+        doReturn(SearchProductResponse(listOf(product1, product2, product3))).whenever(catalogApi).searchProduct(any())
+
+        assertEndpointEquals("/screens/settings/store/store.json", url)
+    }
 }
