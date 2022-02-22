0d077cd8e6ac90c37053d6f5a3d8b69540da373b
wutsi <herve.tchepannou@wutsi.com>
2022-02-22 18:24:47 -0500

    improve ui

--- src/main/kotlin/com/wutsi/application/store/endpoint/cart/screen/CartScreen.kt
+++ src/main/kotlin/com/wutsi/application/store/endpoint/cart/screen/CartScreen.kt
@@ -1,9 +1,12 @@
 package com.wutsi.application.store.endpoint.cart.screen
 
 import com.wutsi.application.shared.Theme
+import com.wutsi.application.shared.model.ActionModel
 import com.wutsi.application.shared.service.SharedUIMapper
 import com.wutsi.application.shared.service.TenantProvider
 import com.wutsi.application.shared.service.URLBuilder
+import com.wutsi.application.shared.ui.CartItemListItem
+import com.wutsi.application.shared.ui.PriceSummaryCard
 import com.wutsi.application.shared.ui.ProfileListItem
 import com.wutsi.application.store.endpoint.AbstractQuery
 import com.wutsi.application.store.endpoint.Page
@@ -13,35 +16,23 @@ import com.wutsi.ecommerce.catalog.WutsiCatalogApi
 import com.wutsi.ecommerce.catalog.dto.ProductSummary
 import com.wutsi.ecommerce.catalog.dto.SearchProductRequest
 import com.wutsi.flutter.sdui.AppBar
-import com.wutsi.flutter.sdui.Button
 import com.wutsi.flutter.sdui.Column
 import com.wutsi.flutter.sdui.Container
 import com.wutsi.flutter.sdui.Divider
-import com.wutsi.flutter.sdui.DropdownButton
-import com.wutsi.flutter.sdui.DropdownMenuItem
-import com.wutsi.flutter.sdui.Icon
-import com.wutsi.flutter.sdui.Image
-import com.wutsi.flutter.sdui.ListItem
-import com.wutsi.flutter.sdui.Row
 import com.wutsi.flutter.sdui.Screen
 import com.wutsi.flutter.sdui.SingleChildScrollView
 import com.wutsi.flutter.sdui.Text
 import com.wutsi.flutter.sdui.Widget
 import com.wutsi.flutter.sdui.WidgetAware
 import com.wutsi.flutter.sdui.enums.Alignment
-import com.wutsi.flutter.sdui.enums.ButtonType
 import com.wutsi.flutter.sdui.enums.CrossAxisAlignment
 import com.wutsi.flutter.sdui.enums.MainAxisAlignment
-import com.wutsi.flutter.sdui.enums.MainAxisSize
-import com.wutsi.flutter.sdui.enums.TextAlignment
-import com.wutsi.flutter.sdui.enums.TextDecoration
 import com.wutsi.platform.account.WutsiAccountApi
 import com.wutsi.platform.tenant.dto.Tenant
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.text.DecimalFormat
 
 @RestController
 @RequestMapping("/cart")
@@ -105,7 +96,8 @@ class CartScreen(
                     )
                 )
             )
-            products.map { toItemWidget(cart, it, tenant) }
+            products.map { toCartItemWidget(cart, it, tenant) }
+                .filterNotNull()
                 .forEach {
                     children.add(it)
                     children.add(Divider(color = Theme.COLOR_DIVIDER))
@@ -146,159 +138,33 @@ class CartScreen(
         ).toWidget()
     }
 
-    private fun toItemWidget(cart: Cart, product: ProductSummary, tenant: Tenant): WidgetAware {
-        val quantity = getQuantity(cart, product.id)
-        val maxQuantity = product.maxOrder ?: product.quantity
-        val fmt = DecimalFormat(tenant.monetaryFormat)
+    private fun toCartItemWidget(cart: Cart, product: ProductSummary, tenant: Tenant): WidgetAware? {
+        val cartItem = cart.products.find { it.productId == product.id }
+            ?: return null
 
-        return Container(
-            child = Column(
-                mainAxisAlignment = MainAxisAlignment.start,
-                crossAxisAlignment = CrossAxisAlignment.start,
-                mainAxisSize = MainAxisSize.min,
-                children = listOf(
-                    ListItem(
-                        caption = product.title,
-                        subCaption = null,
-                        leading = product.thumbnail?.let { Image(url = it.url, width = 48.0, height = 48.8) },
-                        trailing = Column(
-                            mainAxisAlignment = MainAxisAlignment.start,
-                            crossAxisAlignment = CrossAxisAlignment.end,
-                            children = listOfNotNull(
-                                Text(
-                                    caption = fmt.format(quantity * (product.price ?: 0.0)),
-                                    bold = true
-                                ),
-                                if (product.comparablePrice != null)
-                                    Text(
-                                        fmt.format(quantity * product.comparablePrice!!),
-                                        size = Theme.TEXT_SIZE_SMALL,
-                                        decoration = TextDecoration.Strikethrough
-                                    )
-                                else
-                                    null,
-                                if (quantity > 1)
-                                    Text(
-                                        getText("page.cart.price_each", arrayOf(fmt.format(product.price))),
-                                        size = Theme.TEXT_SIZE_SMALL,
-                                    )
-                                else
-                                    null
-                            )
-                        )
-                    ),
-                    Row(
-                        mainAxisAlignment = MainAxisAlignment.start,
-                        crossAxisAlignment = CrossAxisAlignment.center,
-                        children = listOf(
-                            Container(padding = 10.0),
-                            Container(
-                                padding = 10.0,
-                                width = 100.0,
-                                child = if (product.quantity > 0)
-                                    DropdownButton(
-                                        name = "quantity",
-                                        value = quantity.toString(),
-                                        children = IntRange(1, maxQuantity).map {
-                                            DropdownMenuItem(caption = it.toString(), value = it.toString())
-                                        },
-                                        outlinedBorder = false,
-                                        action = executeCommand(
-                                            urlBuilder.build("commands/update-cart?merchant-id=${product.accountId}&product-id=${product.id}")
-                                        )
-                                    )
-                                else
-                                    Row(
-                                        children = listOf(
-                                            Icon(code = Theme.ICON_CANCEL, color = Theme.COLOR_DANGER),
-                                            Text(
-                                                getText("page.cart.out-of-stock"),
-                                                color = Theme.COLOR_DANGER,
-                                                size = Theme.TEXT_SIZE_SMALL
-                                            )
-                                        )
-                                    )
-                            ),
-                            Button(
-                                padding = 10.0,
-                                stretched = false,
-                                type = ButtonType.Text,
-                                caption = getText("page.cart.button.remove"),
-                                action = executeCommand(
-                                    urlBuilder.build("commands/remove-from-cart?merchant-id=${product.accountId}&product-id=${product.id}")
-                                )
-                            )
-                        )
-                    )
+        return CartItemListItem(
+            model = sharedUIMapper.toCartItemModel(cartItem, product, tenant),
+            changeQuantityAction = ActionModel(
+                caption = "",
+                action = executeCommand(
+                    urlBuilder.build("commands/update-cart?merchant-id=${product.accountId}&product-id=${product.id}")
                 )
-            )
-        )
-    }
-
-    private fun toPriceWidget(cart: Cart, products: List<ProductSummary>, tenant: Tenant): WidgetAware {
-        val subTotal = products.sumOf { getQuantity(cart, it.id) * (it.comparablePrice ?: it.price ?: 0.0) }
-        val total = products.sumOf { getQuantity(cart, it.id) * (it.price ?: 0.0) }
-        val savings = total - subTotal
-
-        val fmt = DecimalFormat(tenant.monetaryFormat)
-        return Container(
-            padding = 10.0,
-            margin = 10.0,
-            border = 1.0,
-            borderColor = Theme.COLOR_DIVIDER,
-            borderRadius = 5.0,
-            child = Column(
-                mainAxisAlignment = MainAxisAlignment.start,
-                mainAxisSize = MainAxisSize.min,
-                children = listOfNotNull(
-                    toPriceRow(getText("page.cart.sub-total", arrayOf(cart.products.size)), fmt.format(subTotal)),
-                    if (subTotal > 0)
-                        toPriceRow(
-                            getText("page.cart.savings", arrayOf(cart.products.size)),
-                            "-" + fmt.format(savings),
-                            false,
-                            Theme.COLOR_SUCCESS
-                        )
-                    else
-                        null,
-                    Container(
-                        background = Theme.COLOR_PRIMARY_LIGHT,
-                        child = toPriceRow(getText("page.cart.total"), fmt.format(total), true),
-                    ),
-                    Container(padding = 10.0),
-                    Button(
-                        caption = getText("page.cart.button.checkout"),
-                        action = executeCommand(urlBuilder.build("commands/create-order?merchant-id=${cart.merchantId}"))
-                    )
-                ),
-            )
-        )
-    }
-
-    private fun toPriceRow(
-        name: String,
-        value: String,
-        bold: Boolean = false,
-        color: String? = null
-    ) = Row(
-        children = listOf(
-            Container(
-                padding = 10.0,
-                child = Text(name, bold = bold),
             ),
-            Container(
-                padding = 10.0,
-                child = Text(
-                    value,
-                    bold = bold,
-                    color = color,
-                    alignment = TextAlignment.Right,
+            removeAction = ActionModel(
+                caption = getText("page.cart.button.remove"),
+                executeCommand(
+                    urlBuilder.build("commands/remove-from-cart?merchant-id=${product.accountId}&product-id=${product.id}")
                 )
             )
-        ),
-        mainAxisAlignment = MainAxisAlignment.spaceBetween
-    )
+        )
+    }
 
-    private fun getQuantity(cart: Cart, productId: Long): Int =
-        cart.products.find { it.productId == productId }?.quantity ?: 0
+    private fun toPriceWidget(cart: Cart, products: List<ProductSummary>, tenant: Tenant): WidgetAware =
+        PriceSummaryCard(
+            model = sharedUIMapper.toPriceSummaryModel(cart, products, tenant),
+            action = ActionModel(
+                caption = getText("page.cart.button.checkout"),
+                action = executeCommand(urlBuilder.build("commands/create-order?merchant-id=${cart.merchantId}"))
+            )
+        )
 }
--- src/main/kotlin/com/wutsi/application/store/endpoint/checkout/screen/CheckoutReviewScreen.kt
+++ src/main/kotlin/com/wutsi/application/store/endpoint/checkout/screen/CheckoutReviewScreen.kt
@@ -1,10 +1,13 @@
 package com.wutsi.application.store.endpoint.checkout.screen
 
 import com.wutsi.application.shared.Theme
+import com.wutsi.application.shared.model.ActionModel
 import com.wutsi.application.shared.service.SecurityContext
 import com.wutsi.application.shared.service.SharedUIMapper
 import com.wutsi.application.shared.service.TenantProvider
 import com.wutsi.application.shared.service.URLBuilder
+import com.wutsi.application.shared.ui.OrderItemListItem
+import com.wutsi.application.shared.ui.PriceSummaryCard
 import com.wutsi.application.shared.ui.ProfileListItem
 import com.wutsi.application.store.endpoint.AbstractQuery
 import com.wutsi.application.store.endpoint.Page
@@ -15,14 +18,10 @@ import com.wutsi.ecommerce.order.WutsiOrderApi
 import com.wutsi.ecommerce.order.dto.Order
 import com.wutsi.ecommerce.order.dto.OrderItem
 import com.wutsi.flutter.sdui.AppBar
-import com.wutsi.flutter.sdui.Button
 import com.wutsi.flutter.sdui.Column
 import com.wutsi.flutter.sdui.Container
 import com.wutsi.flutter.sdui.Divider
 import com.wutsi.flutter.sdui.IconButton
-import com.wutsi.flutter.sdui.Image
-import com.wutsi.flutter.sdui.ListItem
-import com.wutsi.flutter.sdui.Row
 import com.wutsi.flutter.sdui.Screen
 import com.wutsi.flutter.sdui.SingleChildScrollView
 import com.wutsi.flutter.sdui.Text
@@ -30,9 +29,6 @@ import com.wutsi.flutter.sdui.Widget
 import com.wutsi.flutter.sdui.WidgetAware
 import com.wutsi.flutter.sdui.enums.CrossAxisAlignment
 import com.wutsi.flutter.sdui.enums.MainAxisAlignment
-import com.wutsi.flutter.sdui.enums.MainAxisSize
-import com.wutsi.flutter.sdui.enums.TextAlignment
-import com.wutsi.flutter.sdui.enums.TextDecoration
 import com.wutsi.platform.account.WutsiAccountApi
 import com.wutsi.platform.tenant.dto.Tenant
 import org.springframework.beans.factory.annotation.Value
@@ -138,104 +134,23 @@ class CheckoutReviewScreen(
         ).toWidget()
     }
 
-    private fun toItemWidget(item: OrderItem, product: ProductSummary, tenant: Tenant): WidgetAware {
-        val fmt = DecimalFormat(tenant.monetaryFormat)
-        return ListItem(
-            caption = product.title,
-            subCaption = getText("page.checkout.review.quantity", arrayOf(item.quantity)),
-            leading = product.thumbnail?.let { Image(url = it.url, width = 48.0, height = 48.8) },
-            trailing = Column(
-                mainAxisAlignment = MainAxisAlignment.start,
-                crossAxisAlignment = CrossAxisAlignment.end,
-                children = listOfNotNull(
-                    Text(fmt.format(item.quantity * item.unitPrice), bold = true),
-                    if (item.unitComparablePrice != null)
-                        Text(
-                            fmt.format(item.quantity * item.unitComparablePrice!!),
-                            size = Theme.TEXT_SIZE_SMALL,
-                            decoration = TextDecoration.Strikethrough
-                        )
-                    else
-                        null,
-                    if (item.quantity > 1)
-                        Text(
-                            getText("page.checkout.review.price_each", arrayOf(fmt.format(item.unitPrice))),
-                            size = Theme.TEXT_SIZE_SMALL,
-                        )
-                    else
-                        null
-                )
-            )
-        )
-    }
+    private fun toItemWidget(item: OrderItem, product: ProductSummary, tenant: Tenant) = OrderItemListItem(
+        model = sharedUIMapper.toOrderItemModel(item, product, tenant)
+    )
 
-    private fun toPriceWidget(order: Order, tenant: Tenant): WidgetAware {
-        val fmt = DecimalFormat(tenant.monetaryFormat)
-        return Container(
-            padding = 10.0,
-            border = 1.0,
-            borderColor = Theme.COLOR_DIVIDER,
-            borderRadius = 2.0,
-            margin = 10.0,
-            child = Column(
-                mainAxisAlignment = MainAxisAlignment.start,
-                mainAxisSize = MainAxisSize.min,
-                children = listOfNotNull(
-                    toPriceRow(
-                        getText("page.checkout.review.sub-total-price", arrayOf(order.items.size)),
-                        fmt.format(order.subTotalPrice)
-                    ),
-                    toPriceRow(getText("page.checkout.review.delivery-fees"), fmt.format(order.deliveryFees)),
-                    if (order.savingsAmount > 0)
-                        toPriceRow(
-                            getText("page.checkout.review.savings"),
-                            "-" + fmt.format(order.savingsAmount),
-                            false,
-                            Theme.COLOR_SUCCESS
-                        )
-                    else
-                        null,
-                    Container(
-                        background = Theme.COLOR_PRIMARY_LIGHT,
-                        child = toPriceRow(
-                            getText("page.checkout.review.total-price"),
-                            fmt.format(order.totalPrice),
-                            true,
-                            Theme.COLOR_PRIMARY
-                        ),
-                    ),
-                    Container(padding = 10.0),
-                    Button(
-                        caption = getText("page.checkout.review.button.pay", arrayOf(fmt.format(order.totalPrice))),
-                        action = gotoUrl(urlBuilder.build(loginUrl, getPaymentUrl(order.id)))
-                    )
+    private fun toPriceWidget(order: Order, tenant: Tenant) = PriceSummaryCard(
+        model = sharedUIMapper.toPriceSummaryModel(order, tenant),
+        action = ActionModel(
+            caption = getText(
+                "page.checkout.review.button.pay",
+                arrayOf(
+                    DecimalFormat(tenant.monetaryFormat).format(order.totalPrice)
                 )
             ),
-        )
-    }
-
-    private fun toPriceRow(
-        name: String,
-        value: String,
-        bold: Boolean = false,
-        color: String? = null
-    ) = Row(
-        children = listOf(
-            Container(
-                padding = 10.0,
-                child = Text(name, bold = bold),
-            ),
-            Container(
-                padding = 10.0,
-                child = Text(
-                    value,
-                    bold = bold,
-                    color = color,
-                    alignment = TextAlignment.Right,
-                )
+            action = gotoUrl(
+                url = urlBuilder.build(loginUrl, getPaymentUrl(order.id))
             )
-        ),
-        mainAxisAlignment = MainAxisAlignment.spaceBetween
+        )
     )
 
     private fun getPaymentUrl(orderId: String): String {
