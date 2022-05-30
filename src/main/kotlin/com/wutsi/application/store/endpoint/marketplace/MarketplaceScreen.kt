742c4cd949ca65967ac0c94c5eba78edf6cf078a
wutsi <herve.tchepannou@wutsi.com>
2022-05-30 02:44:16 -0400

    show merchants on the marketplace

--- src/main/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreen.kt
+++ src/main/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreen.kt
@@ -1,26 +1,29 @@
 package com.wutsi.application.store.endpoint.marketplace
 
 import com.wutsi.application.shared.Theme
+import com.wutsi.application.shared.ui.ProfileListItem
 import com.wutsi.application.store.endpoint.AbstractQuery
 import com.wutsi.application.store.endpoint.Page
 import com.wutsi.ecommerce.catalog.WutsiCatalogApi
-import com.wutsi.ecommerce.catalog.dto.CategorySummary
-import com.wutsi.ecommerce.catalog.dto.SearchCategoryRequest
+import com.wutsi.ecommerce.catalog.dto.SearchMerchantRequest
 import com.wutsi.flutter.sdui.AppBar
 import com.wutsi.flutter.sdui.Center
 import com.wutsi.flutter.sdui.Column
 import com.wutsi.flutter.sdui.Container
 import com.wutsi.flutter.sdui.Divider
-import com.wutsi.flutter.sdui.Flexible
-import com.wutsi.flutter.sdui.Icon
-import com.wutsi.flutter.sdui.ListItem
-import com.wutsi.flutter.sdui.ListView
 import com.wutsi.flutter.sdui.Screen
+import com.wutsi.flutter.sdui.SingleChildScrollView
 import com.wutsi.flutter.sdui.Text
 import com.wutsi.flutter.sdui.Widget
 import com.wutsi.flutter.sdui.WidgetAware
 import com.wutsi.flutter.sdui.enums.CrossAxisAlignment
 import com.wutsi.flutter.sdui.enums.MainAxisAlignment
+import com.wutsi.platform.account.WutsiAccountApi
+import com.wutsi.platform.account.dto.AccountSummary
+import com.wutsi.platform.account.dto.Category
+import com.wutsi.platform.account.dto.SearchAccountRequest
+import com.wutsi.platform.account.entity.AccountSort
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -29,14 +32,32 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/marketplace")
 class MarketplaceScreen(
     private val catalogApi: WutsiCatalogApi,
+    private val accountApi: WutsiAccountApi,
+    @Value("\${wutsi.application.shell-url}") shellUrl: String,
 ) : AbstractQuery() {
     @PostMapping
     fun index(): Widget {
-        val categories = catalogApi.searchCategories(
-            request = SearchCategoryRequest(
-                parentId = null,
+        // Get merchants
+        val merchants = catalogApi.searchMerchants(
+            request = SearchMerchantRequest(
+                withPublishedProducts = true,
+                limit = 30,
             )
-        ).categories.filter { it.publishedProductCount > 0 }
+        ).merchants
+
+        // Get stores
+        val stores = if (merchants.isEmpty())
+            emptyList()
+        else
+            accountApi.searchAccount(
+                request = SearchAccountRequest(
+                    ids = merchants.map { it.accountId },
+                    sortBy = AccountSort.NAME.name
+                )
+            ).accounts
+
+        // Get the categories
+        val categories = accountApi.listCategories().categories
 
         return Screen(
             id = Page.MARKETPLACE,
@@ -47,47 +68,57 @@ class MarketplaceScreen(
                 title = getText("page.marketplace.app-bar.title"),
             ),
             bottomNavigationBar = bottomNavigationBar(),
-            child = Container(
-                child = if (categories.isNotEmpty())
-                    toCategoyListWidget(categories)
-                else
+            backgroundColor = Theme.COLOR_GRAY_LIGHT,
+            child = SingleChildScrollView(
+                child = if (stores.isEmpty())
                     Center(
                         Container(
                             padding = 10.0,
                             child = Text(getText("page.marketplace.empty"))
                         )
                     )
+                else
+                    toStoreListWidget(stores, categories),
             ),
         ).toWidget()
     }
 
-    private fun toCategoyListWidget(categories: List<CategorySummary>): WidgetAware =
-        Column(
-            crossAxisAlignment = CrossAxisAlignment.start,
-            mainAxisAlignment = MainAxisAlignment.start,
-            children = listOf(
-                Center(
-                    Container(
-                        padding = 10.0,
-                        child = Text(getText("page.marketplace.browse-categories"))
-                    )
-                ),
-                Divider(height = 1.0, color = Theme.COLOR_DIVIDER),
-                Flexible(
-                    child = ListView(
-                        separator = true,
-                        separatorColor = Theme.COLOR_DIVIDER,
-                        children = categories.map {
-                            ListItem(
-                                caption = it.title,
-                                trailing = Icon(Theme.ICON_CHEVRON_RIGHT),
-                                action = gotoUrl(
-                                    url = urlBuilder.build("/marketplace/category?id=${it.id}")
-                                )
-                            )
-                        },
-                    )
+    private fun toStoreListWidget(stores: List<AccountSummary>, categories: List<Category>): WidgetAware {
+        val categoryMap = categories.associateBy { it.id }
+        val children = mutableListOf<WidgetAware>()
+        children.add(
+            Center(
+                Container(
+                    padding = 10.0,
+                    child = Text(getText("page.marketplace.stores"), bold = true)
                 )
-            )
+            ),
         )
+        children.addAll(
+            stores.flatMap {
+                listOf(
+                    Divider(height = 1.0, color = Theme.COLOR_DIVIDER),
+                    ProfileListItem(
+                        model = sharedUIMapper.toAccountModel(
+                            it,
+                            category = if (it.categoryId == null) null else categoryMap[it.categoryId],
+                        ),
+                        action = gotoUrl(urlBuilder.build(shellUrl, "/profile?id=${it.id}&tab=store")),
+                        showAccountType = false,
+                        showLocation = true
+                    ),
+                )
+            }
+        )
+        return Container(
+            margin = 5.0,
+            background = Theme.COLOR_WHITE,
+            borderColor = Theme.COLOR_DIVIDER,
+            child = Column(
+                crossAxisAlignment = CrossAxisAlignment.start,
+                mainAxisAlignment = MainAxisAlignment.start,
+                children = children
+            ),
+        )
+    }
 }
--- src/test/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreenTest.kt
+++ src/test/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreenTest.kt
@@ -4,8 +4,12 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.application.store.endpoint.AbstractEndpointTest
-import com.wutsi.ecommerce.catalog.dto.CategorySummary
-import com.wutsi.ecommerce.catalog.dto.SearchCategoryResponse
+import com.wutsi.ecommerce.catalog.dto.MerchantSummary
+import com.wutsi.ecommerce.catalog.dto.SearchMerchantResponse
+import com.wutsi.platform.account.dto.AccountSummary
+import com.wutsi.platform.account.dto.Category
+import com.wutsi.platform.account.dto.ListCategoryResponse
+import com.wutsi.platform.account.dto.SearchAccountResponse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -27,14 +31,30 @@ internal class MarketplaceScreenTest : AbstractEndpointTest() {
 
     @Test
     fun index() {
-        val categories = listOf(
-            CategorySummary(id = 1, title = "Cat 1", publishedProductCount = 10),
-            CategorySummary(id = 2, title = "Cat 2", publishedProductCount = 10),
-            CategorySummary(id = 3, title = "Cat 3", publishedProductCount = 0),
-            CategorySummary(id = 4, title = "Cat 4", publishedProductCount = 0),
+        // GIVEN
+        val merchants = listOf(
+            MerchantSummary(accountId = 111),
+            MerchantSummary(accountId = 222),
+            MerchantSummary(accountId = 333)
         )
-        doReturn(SearchCategoryResponse(categories)).whenever(catalogApi).searchCategories(any())
+        doReturn(SearchMerchantResponse(merchants)).whenever(catalogApi).searchMerchants(any())
 
+        val stores = listOf(
+            AccountSummary(id = 111, business = true, categoryId = 111),
+            AccountSummary(id = 222, business = true, categoryId = 222),
+            AccountSummary(id = 333, business = true, categoryId = 333)
+        )
+        doReturn(SearchAccountResponse(stores)).whenever(accountApi).searchAccount(any())
+
+        val categories = listOf(
+            Category(id = 1, title = "Cat 1"),
+            Category(id = 2, title = "Cat 2"),
+            Category(id = 3, title = "Cat 3"),
+            Category(id = 4, title = "Cat 4"),
+        )
+        doReturn(ListCategoryResponse(categories)).whenever(accountApi).listCategories()
+
+        // WHEN
         assertEndpointEquals("/screens/marketplace/marketplace.json", url)
     }
 }
