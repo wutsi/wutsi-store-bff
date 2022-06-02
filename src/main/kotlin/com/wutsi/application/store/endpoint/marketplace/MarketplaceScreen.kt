b97d42bb2e3d7a05ccb2e1cf22fcb703a3e97f85
wutsi <herve.tchepannou@wutsi.com>
2022-06-02 12:30:38 -0400

    show opened cart

--- src/main/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreen.kt
+++ src/main/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreen.kt
@@ -4,13 +4,18 @@ import com.wutsi.application.shared.Theme
 import com.wutsi.application.shared.ui.ProfileListItem
 import com.wutsi.application.store.endpoint.AbstractQuery
 import com.wutsi.application.store.endpoint.Page
+import com.wutsi.ecommerce.cart.dto.CartSummary
+import com.wutsi.ecommerce.cart.dto.SearchCartRequest
 import com.wutsi.ecommerce.catalog.WutsiCatalogApi
 import com.wutsi.ecommerce.catalog.dto.SearchMerchantRequest
+import com.wutsi.ecommerce.catalog.dto.SearchProductRequest
 import com.wutsi.flutter.sdui.AppBar
 import com.wutsi.flutter.sdui.Center
 import com.wutsi.flutter.sdui.Column
 import com.wutsi.flutter.sdui.Container
 import com.wutsi.flutter.sdui.Divider
+import com.wutsi.flutter.sdui.Image
+import com.wutsi.flutter.sdui.Row
 import com.wutsi.flutter.sdui.Screen
 import com.wutsi.flutter.sdui.SingleChildScrollView
 import com.wutsi.flutter.sdui.Text
@@ -23,6 +28,7 @@ import com.wutsi.platform.account.dto.AccountSummary
 import com.wutsi.platform.account.dto.Category
 import com.wutsi.platform.account.dto.SearchAccountRequest
 import com.wutsi.platform.account.entity.AccountSort
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -33,10 +39,17 @@ import org.springframework.web.bind.annotation.RestController
 class MarketplaceScreen(
     private val catalogApi: WutsiCatalogApi,
     private val accountApi: WutsiAccountApi,
+
     @Value("\${wutsi.application.shell-url}") shellUrl: String,
 ) : AbstractQuery() {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(MarketplaceScreen::class.java)
+    }
+
     @PostMapping
     fun index(): Widget {
+        val children = mutableListOf<WidgetAware>()
+
         // Get merchants
         val merchants = catalogApi.searchMerchants(
             request = SearchMerchantRequest(
@@ -56,8 +69,22 @@ class MarketplaceScreen(
                 )
             ).accounts
 
-        // Get the categories
-        val categories = accountApi.listCategories().categories
+        if (stores.isNotEmpty()) {
+            // Categories
+            val categories = accountApi.listCategories().categories
+
+            // Opened Carts
+            val carts = getOpenedCarts()
+            val cartWidget = toCartListWidget(carts, stores, categories)
+            if (cartWidget != null)
+                children.add(cartWidget)
+
+            // Stores
+            if (children.isNotEmpty()) {
+                children.add(Container(padding = 10.0))
+            }
+            children.add(toStoreListWidget(stores, categories))
+        }
 
         return Screen(
             id = Page.MARKETPLACE,
@@ -77,8 +104,13 @@ class MarketplaceScreen(
                             child = Text(getText("page.marketplace.empty"))
                         )
                     )
-                else
-                    toStoreListWidget(stores, categories),
+                else {
+                    Column(
+                        mainAxisAlignment = MainAxisAlignment.start,
+                        crossAxisAlignment = CrossAxisAlignment.start,
+                        children = children
+                    )
+                }
             ),
         ).toWidget()
     }
@@ -110,15 +142,141 @@ class MarketplaceScreen(
                 )
             }
         )
-        return Container(
-            margin = 5.0,
-            background = Theme.COLOR_WHITE,
-            borderColor = Theme.COLOR_DIVIDER,
+        return toSectionWidget(
+            padding = null,
             child = Column(
                 crossAxisAlignment = CrossAxisAlignment.start,
                 mainAxisAlignment = MainAxisAlignment.start,
                 children = children
             ),
         )
+    }
+
+    private fun toCartListWidget(
+        carts: List<CartSummary>,
+        stores: List<AccountSummary>,
+        categories: List<Category>
+    ): WidgetAware? {
+        if (carts.isEmpty())
+            return null
+
+        val categoryMap = categories.associateBy { it.id }
+        val storeMap = stores.associateBy { it.id }.toMutableMap()
+        val children = mutableListOf<WidgetAware>()
+        carts.forEach {
+            val cart = toCartWidget(it, storeMap, categoryMap)
+            if (cart != null) {
+                children.add(Divider(height = 1.0, color = Theme.COLOR_DIVIDER))
+                children.add(cart)
+            }
+        }
+        if (children.isEmpty())
+            return null
+
+        children.add(
+            0, Center(
+                Container(
+                    padding = 10.0,
+                    child = Text(getText("page.marketplace.carts"), bold = true)
+                )
+            )
+        )
+        return toSectionWidget(
+            padding = null,
+            child = Column(
+                crossAxisAlignment = CrossAxisAlignment.start,
+                mainAxisAlignment = MainAxisAlignment.start,
+                children = children
+            ),
+        )
+    }
+
+    private fun toCartWidget(
+        cart: CartSummary,
+        storeMap: MutableMap<Long, AccountSummary>,
+        categoryMap: Map<Long, Category>
+    ): WidgetAware? {
+        try {
+            val store = getStore(cart, storeMap) ?: return null
+            val cart = cartApi.getCart(store.id).cart
+            val products = catalogApi.searchProducts(
+                request = SearchProductRequest(
+                    productIds = cart.products.map { it.productId },
+                    limit = 7
+                )
+            ).products
+            if (products.isEmpty())
+                return null
+
+            return Container(
+                child = Column(
+                    mainAxisAlignment = MainAxisAlignment.start,
+                    crossAxisAlignment = CrossAxisAlignment.start,
+                    children = listOfNotNull(
+                        ProfileListItem(
+                            model = sharedUIMapper.toAccountModel(
+                                store,
+                                category = if (store.categoryId == null) null else categoryMap[store.categoryId],
+                            ),
+                            action = gotoUrl(urlBuilder.build(shellUrl, "/profile?id=${store.id}&tab=store")),
+                            showAccountType = false,
+                            showLocation = true
+                        ),
+                        Row(
+                            mainAxisAlignment = MainAxisAlignment.start,
+                            crossAxisAlignment = CrossAxisAlignment.start,
+                            children = products.map {
+                                Container(
+                                    padding = 5.0,
+                                    child = Image(
+                                        url = it.thumbnail?.url ?: "",
+                                        width = 24.0,
+                                        height = 24.0
+                                    )
+                                )
+                            }
+                        )
+                    )
+                )
+            )
+
+        } catch (ex: Exception) {
+            LOGGER.warn("Unexpected error", ex)
+            return null
+        }
+    }
+
+    private fun getOpenedCarts(): List<CartSummary> =
+        try {
+            if (togglesProvider.isCartEnabled())
+                cartApi.searchCarts(
+                    request = SearchCartRequest(
+                        accountId = securityContext.currentAccountId(),
+                        limit = 3
+                    )
+                ).carts
+            else
+                emptyList()
+        } catch (ex: Exception) {
+            LOGGER.warn("Unable to resolve the opened carts", ex)
+            emptyList<CartSummary>()
+        }
+
+    private fun getStore(cart: CartSummary, storeMap: MutableMap<Long, AccountSummary>): AccountSummary? {
+        val merchant = storeMap[cart.merchantId]
+        return merchant
+            ?: loadStore(cart.merchantId, storeMap)
+    }
+
+    private fun loadStore(merchantId: Long, merchantMap: MutableMap<Long, AccountSummary>): AccountSummary? {
+        val merchants = accountApi.searchAccount(
+            SearchAccountRequest(ids = listOf(merchantId))
+        ).accounts
+
+        if (merchants.isNotEmpty()) {
+            merchantMap[merchantId] = merchants[0]
+            return merchants[0]
+        }
+        return null
     }
 }
--- src/test/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreenTest.kt
+++ src/test/kotlin/com/wutsi/application/store/endpoint/marketplace/MarketplaceScreenTest.kt
@@ -4,8 +4,17 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.application.store.endpoint.AbstractEndpointTest
+import com.wutsi.ecommerce.cart.WutsiCartApi
+import com.wutsi.ecommerce.cart.dto.Cart
+import com.wutsi.ecommerce.cart.dto.CartSummary
+import com.wutsi.ecommerce.cart.dto.GetCartResponse
+import com.wutsi.ecommerce.cart.dto.Product
+import com.wutsi.ecommerce.cart.dto.SearchCartResponse
 import com.wutsi.ecommerce.catalog.dto.MerchantSummary
+import com.wutsi.ecommerce.catalog.dto.PictureSummary
+import com.wutsi.ecommerce.catalog.dto.ProductSummary
 import com.wutsi.ecommerce.catalog.dto.SearchMerchantResponse
+import com.wutsi.ecommerce.catalog.dto.SearchProductResponse
 import com.wutsi.platform.account.dto.AccountSummary
 import com.wutsi.platform.account.dto.Category
 import com.wutsi.platform.account.dto.ListCategoryResponse
@@ -13,6 +22,7 @@ import com.wutsi.platform.account.dto.SearchAccountResponse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.web.server.LocalServerPort
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,41 +30,78 @@ internal class MarketplaceScreenTest : AbstractEndpointTest() {
     @LocalServerPort
     val port: Int = 0
 
+    @MockBean
+    private lateinit var cartApi: WutsiCartApi
+
     private lateinit var url: String
+
+    val merchants = listOf(
+        MerchantSummary(accountId = 111),
+        MerchantSummary(accountId = 222),
+        MerchantSummary(accountId = 333)
+    )
+
+    val stores = listOf(
+        AccountSummary(id = 111, business = true, categoryId = 111),
+        AccountSummary(id = 222, business = true, categoryId = 222),
+        AccountSummary(id = 333, business = true, categoryId = 333)
+    )
+
+    val categories = listOf(
+        Category(id = 1, title = "Cat 1"),
+        Category(id = 2, title = "Cat 2"),
+        Category(id = 3, title = "Cat 3"),
+        Category(id = 4, title = "Cat 4"),
+    )
+
 
     @BeforeEach
     override fun setUp() {
         super.setUp()
 
         url = "http://localhost:$port/marketplace"
+
+        doReturn(SearchMerchantResponse(merchants)).whenever(catalogApi).searchMerchants(any())
+        doReturn(SearchAccountResponse(stores)).whenever(accountApi).searchAccount(any())
+        doReturn(ListCategoryResponse(categories)).whenever(accountApi).listCategories()
     }
 
     @Test
     fun index() {
-        // GIVEN
-        val merchants = listOf(
-            MerchantSummary(accountId = 111),
-            MerchantSummary(accountId = 222),
-            MerchantSummary(accountId = 333)
-        )
-        doReturn(SearchMerchantResponse(merchants)).whenever(catalogApi).searchMerchants(any())
-
-        val stores = listOf(
-            AccountSummary(id = 111, business = true, categoryId = 111),
-            AccountSummary(id = 222, business = true, categoryId = 222),
-            AccountSummary(id = 333, business = true, categoryId = 333)
-        )
-        doReturn(SearchAccountResponse(stores)).whenever(accountApi).searchAccount(any())
-
-        val categories = listOf(
-            Category(id = 1, title = "Cat 1"),
-            Category(id = 2, title = "Cat 2"),
-            Category(id = 3, title = "Cat 3"),
-            Category(id = 4, title = "Cat 4"),
-        )
-        doReturn(ListCategoryResponse(categories)).whenever(accountApi).listCategories()
-
-        // WHEN
         assertEndpointEquals("/screens/marketplace/marketplace.json", url)
+    }
+
+    @Test
+    fun openedCart() {
+        // WHEN
+        val products = listOf(
+            ProductSummary(100, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/0.png")),
+            ProductSummary(101, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/1.png")),
+            ProductSummary(102, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/2.png")),
+            ProductSummary(103, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/3.png")),
+            ProductSummary(104, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/4.png")),
+            ProductSummary(105, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/5.png")),
+            ProductSummary(106, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/6.png")),
+            ProductSummary(107, thumbnail = PictureSummary(id = 100, url = "https://www.g.com/7.png")),
+        )
+        doReturn(SearchProductResponse(products)).whenever(catalogApi).searchProducts(any())
+
+        val carts = listOf(
+            CartSummary(merchantId = 111)
+        )
+        doReturn(SearchCartResponse(carts)).whenever(cartApi).searchCarts(any())
+
+        val cart = Cart(
+            merchantId = 111,
+            products = listOf(
+                Product(productId = 100),
+                Product(productId = 101),
+                Product(productId = 103)
+            )
+        )
+        doReturn(GetCartResponse(cart)).whenever(cartApi).getCart(any())
+
+        // GIVEN
+        assertEndpointEquals("/screens/marketplace/marketplace-opened-cart.json", url)
     }
 }
